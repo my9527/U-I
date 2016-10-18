0f2f44a034cd36b994d661968476b2cb09ac27a8
yl <2199048390@qq.com>
2016-10-19 01:05:32 +0800

    setup( splash ):初始化splash;

---
+++ src/modules/splash/main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by my on 2016/10/19.
+ */
+angular
+    .module("module.splash", ["ngRoute"])
+    .config(["$routeProvider", function($routeProvider){
+        $routeProvider
+            .when("/splash", {
+                templateUrl: "html/splash/splash.html"
+            })
+    }])
+    .directive("mySplash", [
+        "$window",
+        "$timeout",
+        "$location",
+        function($window, $timeout, $location){
+            var t = null;
+            function link($scope, $ele){
+                $timeout(function(){
+                    $ele.css("display", "none")
+                }, 3000)
+            }
+            function compile(ele, attr){
+                return function(){
+                    document.body.appendChild(ele[0]);
+                    t = $timeout(function(){
+
+                        document.body.removeChild(ele[0]);
+                        $timeout.cancel(t);
+                        t = null;
+                    }, 3500);
+                    return{
+                        postlink: link
+                    }
+                }
+
+            }
+            return {
+                templateUrl: "html/splash/splash.html",
+                link: link
+                // compile: compile
+            }
+
+        }
+    ])
